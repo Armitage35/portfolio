5fe981e60903dc9c8a49f0c56c59bf1394653f60
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-27 08:37:51 -0400

    Setup project structure

---
+++ src/Views/Portfolio/Portfolio.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<p>Porfolio</p>
+		<Home></Home>
+	</div>
+</template>
+
+<script>
+import Home from './Home/Home';
+
+export default {
+	components: { Home }
+}
+</script>
---
+++ src/Views/Views.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<p>Views</p>
+		<Portfolio></Portfolio>
+	</div>
+</template>
+
+<script>
+import Portfolio from './Portfolio/Portfolio';
+
+export default {
+	components: { Portfolio }
+}
+</script>
