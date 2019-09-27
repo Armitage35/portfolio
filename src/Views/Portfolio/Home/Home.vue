b49b8c29b4d662553ba1b30d5bccd6958ac1e694
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-27 09:55:17 -0400

    Wrap up home compo

--- src/Views/Portfolio/Home/Home.vue
+++ src/Views/Portfolio/Home/Home.vue
@@ -1,9 +1,19 @@
 <template>
-	<div>Home</div>
+	<div class="portfolio__home">
+		<div class="home__name">
+			<h1>Adrien Dubois</h1>
+			<h1 class="portfolio__lastName">Ahlqvist</h1>
+		</div>
+		<span class="home__legend">Product Owner, Web wizard, Boulderer</span>
+		<p>What does a product owner jack of all trades looks like? Like a whole lot of getting shit done. So, how soon are we shipping great stuff?</p>
+		<div class="home__buttons">
+			<button class="bttn-primary">Portfolio</button>
+			<button class="bttn-secondary">Resume</button>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
