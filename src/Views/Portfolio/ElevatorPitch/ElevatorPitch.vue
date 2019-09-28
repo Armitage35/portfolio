b864ab9cb3d46f00aef57af69ea4e610c5a68a43
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-28 17:35:24 -0400

    Boilerplate skills section

---
+++ src/Views/Portfolio/ElevatorPitch/ElevatorPitch.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="content__elevatorPitch">
+		<h1>My role</h1>
+		<p>As a product owner, my mission is to deliver a great product to the user, to commit developers to technological excellency, to ensure designs are respect constraints, to enable the company to understand what it is we are building, to ensure our representatives are heard.</p>
+		<p>As a manager, I believe that working software is the primary measure of sucess, that maximizing the amount of work not done is essential and that business people and developers must work together daily  throughout the project.</p>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/Views/Portfolio/Skills/Skills.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="content__skills">
+		<h1>Skills</h1>
+		<p>My skills are in design, data science, front and back-end development. Being able to take a product or a feature from idea to adoption is what I thrive for.</p>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
