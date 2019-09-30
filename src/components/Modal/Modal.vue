fc875bc99186f14f7cb548032001febd9452027f
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-30 18:19:12 -0400

    Wrap projects up

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -1,16 +1,15 @@
 <template>
-	<div class="modal__mask">
+	<div class="modal__mask" @keydown.esc="$emit('toggleModal')" tabindex="0">
 		<div class="modal">
 			<div class="modal__header">
 				<div class="modal__title">
 					<h1>{{ content.name }}</h1>
 					<i class="fas fa-times" @click="$emit('toggleModal')"></i>
 				</div>
-				<div class="modal__date" v-if="content.date">{{ content.date }}</div>
-				<div class="modal__date" v-else>{{ content.place }}</div>
+				<div class="modal__date">{{ dateOrName }}</div>
 			</div>
 			<div class="modal__body">
-				<img class="modal__cover" :src="content.cover" />
+				<div class="modal__cover" :style="'background-image: url(' + content.cover + ')'"></div>
 				<div class="modal__text">
 					<p class="modal__description">{{ content.description }}</p>
 					<a class="modal__link" v-if="content.link" :href="content.link" target="_blank">Get to the project</a>
@@ -22,6 +21,11 @@
 
 <script>
 	export default {
-		props: ['content']
+		props: ['content'],
+		computed: {
+			dateOrName: function() {
+				return this.content.date ? this.content.date : this.content.place
+			}
+		}
 	}
 </script>
