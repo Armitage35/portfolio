f7826abf160095bf122a4f2fa4ce6331892c5298
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-01 15:06:23 -0400

    Boilerplate CV

--- src/Views/CV/CV.vue
+++ src/Views/CV/CV.vue
@@ -1,9 +1,28 @@
 <template>
-	<h1>CV</h1>
+	<div class="cv__container">
+		<div class="cv">
+			<div class="cv__header">
+				<div class="header__veryTop">
+					<div class="header__hi">Hello!</div>
+					<div class="header__speech">My name is Adrien Dubois Ahlqvist, I’ve been a product owner, growth & product designer for about four years. I'm a maker, a jack of all trades: I do what needs to be done to create and deploy products that enable users to solve their pains. I’m motivated, dynamic and I'm all about rigorous method. </div>
+				</div>
+				<div class="header__into">adrien dubois ahlqvist, Product owner and Jack of all trades</div>
+			</div>
+			<div class="cv__content">
+				<div class="cv__left">
+					<cvSection></cvSection>
+				</div>
+				<div class="cv__right"></div>
+			</div>
+			<div class="cv__footer"></div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
+	import cvSection from './cvSection/cvSection';
 
-}
+	export default {
+		components: { cvSection }
+	}
 </script>
---
+++ src/Views/CV/cvSection/cvSection.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="cv__section">
+		<div class="section__name">Work<span class="section__nameEmphasis">.</span></div>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
