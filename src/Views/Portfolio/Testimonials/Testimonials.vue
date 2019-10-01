3889c50a8f7fe95bfa429a3a15fb22661cb5fec3
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-30 21:20:26 -0400

    Please linter gods

--- src/Views/Portfolio/Testimonials/Testimonials.vue
+++ src/Views/Portfolio/Testimonials/Testimonials.vue
@@ -15,17 +15,17 @@
 		data: function() {
 			return {
 				testimonials: [{
-					quote: 'I had the immense pleasure of working with Adrien for almost two years, and never once did he fail to impress me with its notoriously wide palette of skills. His great attention to detail, rigorousness and brilliancy made him a reference for everyone working with him. He can adapt, improvise and overcome any problem',
+					quote: 'I had the immense pleasure of working with Adrien for almost two years, and never once did he fail to impress me with his great attention to detail, rigorousness and brilliancy which made him a reference for everyone working with him. He can adapt, improvise and overcome any problem',
 					person: 'Roméo Labonté - Senior Software Architect'
 				},
 				{
-					quote: 'Adrien is the ultimate generalist: a reliable, incredibly adaptive problem-solver you can throw anything at, with an organized, analytical, clever, curious, passionate and ultimately rare mind',
+					quote: 'Adrien has the strength to hold and defend his good opinions and ideas, the knowledge and intelligence to justify them, and yet the humility to appreciate a good counterargument. He is the ultimate generalist: a reliable, incredibly adaptive problem-solver you can throw anything at, with an organized, analytical, clever, curious, passionate and ultimately rare mind',
 					person: 'Simon Abramovitch - Senior SEO Consultant '
 				},
 				{
 					quote: 'His ability to verbalize complex technological issues in such a way as to make them easily understandable was a significant asset. Overall, he was a valued team member who allowed us to successfully create value out of chaos',
 					person: 'Marine Godfroy - Retail Strategist'
-				},]
+				}]
 			}
 		}
 	}
