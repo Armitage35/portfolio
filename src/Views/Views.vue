ed44ce345a6d0d123b4413172679d2122bde41d3
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-09-30 22:22:01 -0400

    Build footer (#18)
    * v0.7.0
    
    * Boilerplate footer
    
    * Wrap up footer style
    
    * Footer should be a component
    
    * Please linter gods
    
    * Last minute change
    
    * Another quick change
    
    * Homage to @nospoone

--- src/Views/Views.vue
+++ src/Views/Views.vue
@@ -1,13 +1,15 @@
 <template>
 	<div>
 		<Portfolio></Portfolio>
+		<Footer></Footer>
 	</div>
 </template>
 
 <script>
 import Portfolio from './Portfolio/Portfolio';
+import Footer from '../components/Footer/Footer';
 
 export default {
-	components: { Portfolio }
+	components: { Portfolio, Footer }
 }
 </script>
