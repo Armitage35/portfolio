e31a1b1d08b8a6503516846e0a29ce78ac89e12d
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-01 13:43:58 -0400

    Populate jobs (#19)
    * v0.8.0
    
    * Make buttons be link
    
    * Improve button look & feel
    
    * Add pictures to jobs
    
    * please linter gods
    
    * Make skill move on hover (cute)
    
    * Add description to jobs
    
    * Update Jogogo picture's so it's full width
    
    * Please linter gods

--- src/Views/Portfolio/ElevatorPitch/ElevatorPitch.vue
+++ src/Views/Portfolio/ElevatorPitch/ElevatorPitch.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="content__elevatorPitch">
+	<div class="content__elevatorPitch" id="portfolio">
 		<h1>My role</h1>
 		<p>As a product owner, my mission is to deliver a great product to the user, to commit developers to technological excellence, to ensure designs are respect constraints, to enable the company to understand what it is we are building, to ensure our representatives are heard. As a manager, I believe that working software is the primary measure of success, that maximizing the amount of work not done is essential and that business people and developers must work together daily throughout the project.</p>
 		<p>As a manager, I believe that working software is the primary measure of success, that maximizing the amount of work not done is essential and that business people and developers must work together daily throughout the project.</p>
--- src/Views/Portfolio/WorkEducation/WorkEducation.vue
+++ src/Views/Portfolio/WorkEducation/WorkEducation.vue
@@ -14,6 +14,12 @@
 <script>
 	import Experience from '../../../components/Experience/Experience';
 
+	// Load cover images for jobs
+	import Jogogo from '../../../assets/work/jogogo.png';
+	import Placeloop from '../../../assets/work/placeloop.jpg';
+	import Pryv from '../../../assets/work/pryv.png';
+	import Tec from '../../../assets/work/tec.jpg';
+
 	export default {
 		components: { Experience },
 		data: function() {
@@ -35,35 +41,34 @@
 					duration: 'Session: 2010-2014',
 					clickable: false
 				}],
-				// @todo: populate work description and pictures
 				work: [{
 					name: 'Product owner',
 					place: 'Jogogo, Montreal',
 					duration: '2018-2019',
-					description: '',
-					cover: '',
+					description: 'Jogogo is a physical space analytics tool that lets its users track and engage their visitors based on their activity. I was the Product Owner for both the App and the API and implemented a SCRUM process that enabled us to complete the redesigns of both our App (React) and our API (GraphQL). Both of the APP and API were used by enterprise clients such as Decathlon, Desjardins, Asmodee, Telus, IBM and Cisco.',
+					cover: Jogogo,
 					clickable: true
 				}, {
 					name: 'Product owner',
 					place: 'Technology Evaluation Centers, Montreal',
 					duration: '2017-2018',
-					description: '',
-					cover: '',
+					description: 'TEC is an enterprise tool that enables enterprises to find the software that best suits their needs. I acted as Product Owner for our website and tools and led a team of twenty people through a technological redesign of our websites and internal tools (ASP.NET and Kentico). I also oversaw and participated in the visual redesign of the website.',
+					cover: Tec,
 					clickable: true
 				},
 				{
 					name: 'Growth hacker & Product manager',
 					place: 'Placeloop, Paris',
 					duration: '2015-2017',
-					description: '',
-					cover: '',
+					description: 'Placeloop is a trade marketing platform that enables brands to provide their resellers with marketing campaigns. I acted as Growth Hacker and Product Manager and was in charge of the app\'s design, implementation, adoption and revenue. Our biggest challenge was a mobile-first redesign of the application (React). Our clients included SAB Miller, Kérastase and L\'Oréal Professionnel.',
+					cover: Placeloop,
 					clickable: true
 				},
 				{
 					name: 'Growth hacker & Product manager',
 					place: 'Pryv, Lausanne',
-					description: '',
-					cover: '',
+					description: 'Pryv is a privacy-focused company that enables life loggers and self quantifiers to store and to access data to make sense of it. I acted as Growth Hacker and Product Manager and was in charge of the Apps\' release and adoption. I was also the point of contact between the business and development teams. Finally, I oversaw partnerships with Moves, Misfits and IFTTT.',
+					cover: Pryv,
 					duration: '2014',
 					clickable: true
 				}],
