4725fe276601b149d31e1e8713291d096bb6eea4
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-02 11:47:03 -0400

    Design nitpicks

--- src/Views/CV/cvSection/cvSection.vue
+++ src/Views/CV/cvSection/cvSection.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="cv__section">
-		<div class="section__name">{{ title }}<span class="section__nameEmphasis">.</span></div>
+		<div class="section__name">{{ title }}<span class="section__nameEmphasis" v-if="title != 'Contact me'">.</span><span class="section__nameEmphasis" v-else>!</span></div>
 		<cvEntry v-for="(entry, key) in content" :content="entry" :key="key" :title="title"></cvEntry>
 	</div>
 </template>
