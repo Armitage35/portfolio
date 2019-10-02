8e3a6ddf625ccfe83415e07559bcf244158954b6
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-02 15:44:41 -0400

    Fix footer

--- src/Views/Views.vue
+++ src/Views/Views.vue
@@ -2,18 +2,18 @@
 	<div>
 		<router-view>
 			<Portfolio></Portfolio>
-			<Footer></Footer>
 			<CV></CV>
 		</router-view>
+		<Footer v-if="$route.path != '/cv'"></Footer>
 	</div>
 </template>
 
 <script>
-import Portfolio from './Portfolio/Portfolio';
-import Footer from '../components/Footer/Footer';
-import CV from './CV/CV';
+	import Portfolio from './Portfolio/Portfolio';
+	import Footer from '../components/Footer/Footer';
+	import CV from './CV/CV';
 
-export default {
-	components: { CV, Portfolio, Footer }
-}
+	export default {
+		components: { CV, Portfolio, Footer }
+	}
 </script>
