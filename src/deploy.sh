39ee4f8019f40af1a2d938f21201e797a0d92a88
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-04 14:52:07 -0400

    Revert "Fix conflict"
    This reverts commit be0c2ce610cf7905f9fe8f0bd4bf64a770a23565.

--- src/deploy.sh
+++ src/deploy.sh
@@ -15,8 +15,6 @@ echo 'armitageweb.net' > CNAME
 git config user.email "adrien.dubois35@gmail.com"
 git config user.name "Adrien D. Ahlqvist"
 
-git config user.email "adrien.dubois35@gmail.com"
-git config user.name "Adrien D. Ahlqvist"
 git init
 git add -A
 git commit -m 'deploy'
