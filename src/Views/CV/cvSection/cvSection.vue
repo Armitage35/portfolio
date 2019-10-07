56c0e1a8c47f1c8ae52441c51fd2d1ef4ab30684
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-07 15:26:01 -0400

    Css for printing (#34)
    * Quick fix on skill's cursor
    
    * v1.4.0
    
    * Implement print specific rules
    
    * Please linter gods

--- src/Views/CV/cvSection/cvSection.vue
+++ src/Views/CV/cvSection/cvSection.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="cv__section">
+	<div :class="className">
 		<div class="section__name">{{ title }}<span class="section__nameEmphasis" v-if="title != 'Contact me'">.</span><span class="section__nameEmphasis" v-else>!</span></div>
 		<cvEntry v-for="(entry, key) in content" :content="entry" :key="key" :title="title"></cvEntry>
 	</div>
@@ -10,6 +10,11 @@
 
 	export default {
 		components: { cvEntry },
-		props: [ 'title', 'content' ]
+		props: [ 'title', 'content' ],
+		computed: {
+			className: function() {
+				return 'cv__section ' + this.title.toLowerCase()
+			}
+		}
 	}
 </script>
