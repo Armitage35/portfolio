0b9078afc881725bd3d457f6f0953cbd2c862598
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-16 11:13:15 -0400

    Add review from Vince (#42)
    * v1.4.2
    
    * Add review by @vlegault

--- src/Views/Portfolio/Testimonials/Testimonials.vue
+++ src/Views/Portfolio/Testimonials/Testimonials.vue
@@ -14,17 +14,18 @@
 		components: { Testimonial },
 		data: function() {
 			return {
-				testimonials: [{
+				testimonials: [
+				{
+					quote: 'Adrien is an awesome person to work with. While we were at Jogogo, he consistently demonstrated a solid work ethic plus a dedication for success. He is what I would call a natural born Product Owner. He embraces the product vision and have great understanding of the market in which the product evolves. He shows great leadership and is always looking for the greater good of his team & product.',
+					person: 'Vincent Legault - Senior Software Architect'
+				},
+				{
 					quote: 'I had the immense pleasure of working with Adrien for almost two years, and never once did he fail to impress me with his great attention to detail, rigorousness and brilliancy which made him a reference for everyone working with him. He can adapt, improvise and overcome any problem',
 					person: 'Roméo Labonté - Senior Software Architect'
 				},
 				{
 					quote: 'Adrien has the strength to hold and defend his good opinions and ideas, the knowledge and intelligence to justify them, and yet the humility to appreciate a good counterargument. He is the ultimate generalist: a reliable, incredibly adaptive problem-solver you can throw anything at, with an organized, analytical, clever, curious, passionate and ultimately rare mind',
 					person: 'Simon Abramovitch - Senior SEO Consultant '
-				},
-				{
-					quote: 'His ability to verbalize complex technological issues in such a way as to make them easily understandable was a significant asset. Overall, he was a valued team member who allowed us to successfully create value out of chaos',
-					person: 'Marine Godfroy - Retail Strategist'
 				}]
 			}
 		}
