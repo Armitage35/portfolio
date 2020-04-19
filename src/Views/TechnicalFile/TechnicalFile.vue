45f69d53cb7dfbe49e74557524a686ef932cf017
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2020-04-19 17:05:38 -0400

    Render all the content we are going to display

--- src/Views/TechnicalFile/TechnicalFile.vue
+++ src/Views/TechnicalFile/TechnicalFile.vue
@@ -1,16 +1,31 @@
 <template>
 	<div class='technicalFile'>
-		<h1>{{strings.en.technicalFile.summary}}</h1>
-		<p v-for="(paragraph, key) in strings.en.portfolio.elevatorPitch.pitch" :key="key">{{paragraph}}</p>
-		<p v-for="(work, key) in strings.en.cv.work" :key="key">{{work.name}}</p>
+		<h1>Summary</h1>
+		<p v-for="(pitchParagraph, key) in strings.en.portfolio.elevatorPitch.pitch" :key="key">
+			{{ pitchParagraph }}
+		</p>
+		<h1>Work</h1>
+		<TechnicalFileWork
+			v-for="(work) in strings.en.cv.work" :key="work.place" :content="work" title="Work"
+		></TechnicalFileWork>
+		<h1>Education</h1>
+		<TechnicalFileWork
+			v-for="(education) in strings.en.cv.education" :key="education.place" :content="education" title="education"
+		></TechnicalFileWork>
+		<h1>Personal projects</h1>
+		<TechnicalFileWork
+			v-for="(project) in strings.en.cv.projects" :key="project.place" :content="project" title="project"
+		></TechnicalFileWork>
 	</div>
 </template>
 
 <script>
+import TechnicalFileWork from './TechnicalFileWork/TechnicalFileWork';
 
 import strings from '../../i18n.json';
 
 export default {
+	components: { TechnicalFileWork },
 	data: function() {
 		return {
 			strings: strings
---
+++ src/Views/TechnicalFile/TechnicalFileWork/TechnicalFileWork.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div class="technicalFileEntry__introduction">
+			<p>{{ content.name }}</p>
+			<p>{{ content.place }} {{ content.country }} </p>
+			<p>{{ content.duration }}</p>
+		</div>
+		<div class="technicalFileEntry__content">
+			<p>{{ content.description }}</p>
+			<p>{{ content.context }}</p>
+			<p>{{ content.appraisal }}</p>
+			<ul v-if="content.results != ''">
+				<li v-for="(results, key) in content.results" :key="key">{{ results }}</li>
+			</ul>
+			<ul v-if="content.technology != ''">
+				<li v-for="(technology, key) in content.tech" :key="key">{{ technology }}</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['content', 'title'],
+};
+</script>
