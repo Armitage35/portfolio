82801b77f8a7edc01ef18891e8322839a265c6c5
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2020-04-20 11:48:07 -0400

    Do not duplicate strings. (#118)
    * v1.10.0
    
    * upgrade packages
    
    * Ask todobot to ignore i18n
    
    * Sass gods, be pleased and be praised
    
    * Remove cv.projects in i18n
    
    * Work is now not duplicated in i18n
    
    * Education is no longer duplicated
    
    * Simplify restricted amount of projects
    
    * Skills are no longer duplicated in i18n
    
    * Fix a bug with the project date/duration
    
    * One should strive for perfection.
    
    * Restyled by prettier (#119)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Update development.yml

--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="project" :style="'background-image: url(' + project.cover + ')'" @click="$emit('seeProject', project)">
-		<div class="project__date">{{ project.date }}</div>
+		<div class="project__date">{{ project.duration }}</div>
 		<div class="project__bottom">
 			<div class="project__name">{{ project.name }}</div>
 			<i class="fas fa-long-arrow-alt-right"></i>
