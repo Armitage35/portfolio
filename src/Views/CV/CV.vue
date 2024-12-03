d88e0e9e5f08963fd13843d58c0057777c33f36a
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2024-12-03 11:25:06 -0500

    revisit wording

--- src/Views/CV/CV.vue
+++ src/Views/CV/CV.vue
@@ -4,9 +4,9 @@
 			<div class="cv__header">
 				<div class="header__veryTop">
 					<div class="header__hi">Hello!</div>
-					<div class="header__speech">My name is Adrien Dubois Ahlqvist, I’ve been a product owner, growth & product designer for about five years. I'm a maker, a jack of all trades: I do what needs to be done to create and deploy products that enable users to solve their pains. I’m motivated, dynamic and I'm all about rigorous method.</div>
+					<div class="header__speech">My name is Adrien Dubois Ahlqvist, I’ve been a product owner, growth & product designer for over ten years. I'm a maker, a jack of all trades: I do what needs to be done to create and deploy products that enable users to solve their pains. I’m motivated, dynamic and I'm all about rigorous method.</div>
 				</div>
-				<div class="header__into">Adrien Dubois Ahlqvist, Product owner and Jack of all trades</div>
+				<div class="header__into">Adrien Dubois Ahlqvist, Senior Product Manager</div>
 			</div>
 			<div class="cv__content">
 				<div class="cv__left">
