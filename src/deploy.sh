872c6d4c3fa48ef2c2565cf0a2a6e449c96a9a9a
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2026-01-22 16:06:52 -0500

    fix: update deploy script to push HEAD to gh-pages branch

--- src/deploy.sh
+++ src/deploy.sh
@@ -20,4 +20,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Armitage35/portfolio.git master:gh-pages
+git push -f git@github.com:Armitage35/portfolio.git HEAD:gh-pages
